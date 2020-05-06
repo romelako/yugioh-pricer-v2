451a6044bc6126aaa41e94810c244f4e76db5339
asianbooshay <64921207+asianbooshay@users.noreply.github.com>
2020-05-06 15:59:14 -0700

    testing

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,6 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('Billy Bob')
+Route.on('/').render('welcome')
+
+Route.get('/', () => 'Billy Bob')
